4ac458dc5a58914328740d0f7ed35c0d898a5e18
unknown <dni.grosu@gmail.com>
2019-10-21 13:18:57 +0300

    final verion

--- app/partial-components/Menu.js
+++ app/partial-components/Menu.js
@@ -6,14 +6,14 @@ const Menu = () => {
   return (
     <ul style={{userSelect: 'none'}}>
       <li className="link">
-        <NavLink tabIndex="2" exact activeClassName="active" to="/">Home</NavLink>
+        <NavLink className="text_link" tabIndex="2" exact activeClassName="active" to="/">Home</NavLink>
       </li>
       <li className="link">
-        <NavLink tabIndex="3" activeClassName="active" to="/todos">To Do's</NavLink>
+        <NavLink className="text_link" tabIndex="3" activeClassName="active" to="/events">Events</NavLink>
       </li>
       <li className="link">
-        <a tabIndex="4" href="https://github.com/httpJunkie/2019-devreach-react-workshop">
-          Source Code <span className="k-icon k-i-hyperlink-open-sm"></span>
+        <a className="text_link" tabIndex="4" href="https://github.com/httpJunkie/2019-devreach-react-workshop">
+          Source Code <i className="k-icon k-i-hyperlink-open-sm"></i>
         </a>
       </li>
       <li className="menu">
--- app/view-components/Events.js
+++ app/view-components/Events.js
@@ -1,29 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Column, Row } from "simple-flexbox";
+
 import EventList from '../partial-components/space-x/EventList';
 import EventDetails from '../partial-components/space-x/EventDetails';
+
+import { useEventData } from '../partial-components/space-x/events-data';
+
 const Events = ({match}) => {
   const event_id = match.params.event_id;
-  const [events, setEvents] = useState([]);
-  const [isloading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setIsLoading(true);
-    fetch(`https://api.spacexdata.com/v3/history`)
-      .then(res => {
-        if(res.ok) {
-          return res.json();
-        } else {
-          throw Error("Fetching Events");
-        }
-      })
-      .then(events => {
-        setEvents(events);
-        setIsLoading(false);
-      })
-      .catch(error => setError(error))
-  },[])
+  const { events, isLoading, error } = useEventData();
 
   useEffect(() => {
     document.title = `SpaceX Historical Events`;
@@ -38,10 +23,8 @@ const Events = ({match}) => {
   const event = events.find(event => {
     return event.id === Number(event_id);
   });
+
   return (
-    // <div className="view-events">
-    //   <h3>Historical Events</h3>
-    // </div>
     <Row horizontal="spaced">
       <Column flexGrow={1} style={{width:'45%', padding: 5}}>
         <EventList eventData={events}/>
@@ -52,4 +35,5 @@ const Events = ({match}) => {
     </Row>
   )
 }
+
 export default Events;
